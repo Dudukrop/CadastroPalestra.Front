2e3f3013177cac6d5d40b187e3b215e4fd4ce0de
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-21 17:53:27 -0300

    borda do mapa alterada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
 				<span>
 					Cabo de Santo Agostinho - PE
 				</span>
-				<div className="mt-3 border border-blue-500 w-fit">
+				<div className="mt-3 border border-indigo-600 w-fit">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.812745758676!2d-34.96663002609654!3d-8.221577991810959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7aae40f04b25577%3A0xecd867230dcd0664!2sAv.%20Ernestina%20Batista%2C%2025%20-%20Pontezinha%2C%20Cabo%20de%20Santo%20Agostinho%20-%20PE%2C%2054589-295!5e0!3m2!1spt-BR!2sbr!4v1713660406172!5m2!1spt-BR!2sbr" loading="lazy"></iframe>
 				</div>
 			</Information>
